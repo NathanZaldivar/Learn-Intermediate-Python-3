60875a2a391dc6ab3d0276ced606589b52694383
Nathan_Zaldivar <nathan.a.zaldivar@gmail.com>
2021-10-07 11:41:17 -0400

    Create unpacking_operator.py

---
+++ FUNCTION ARGUMENTS/unpacking_operator.py
@@ -0,0 +1,13 @@
+# Write your code below: 
+
+
+
+
+def print_order(*order_items):
+# *order_items arguement returns tuple
+  print(order_items)
+
+
+
+print_order("Orange Juice", "Apple Juice", "Scrambled Eggs", "Pancakes")
+
